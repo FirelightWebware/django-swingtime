e5fd4365be39a59b85d5dbdc97077e56b47775d1
katy lavallee <katylava@gmail.com>
2013-08-23 10:16:06 -0500

    Replace conf dir with normal settings file

--- swingtime/settings.py
+++ swingtime/settings.py
@@ -1,16 +1,23 @@
 import datetime
+from django.conf import settings
 
 # A "strftime" string for formatting start and end time selectors in forms
-TIMESLOT_TIME_FORMAT = '%I:%M %p'
+TIMESLOT_TIME_FORMAT = getattr(
+    settings, 'SWINGTIME_TIMESLOT_TIME_FORMAT', '%I:%M %p'
+)
 
 # Used for creating start/end time form selectors as well as time slot grids.
 # Value should be datetime.timedelta value representing the incremental
 # differences between temporal options
-TIMESLOT_INTERVAL = datetime.timedelta(minutes=15)
+TIMESLOT_INTERVAL = (
+    settings, 'SWINGTIME_TIMESLOT_INTERVAL' , datetime.timedelta(minutes=15)
+)
 
 # A datetime.time value indicting the starting time for time slot grids and form
 # selectors
-TIMESLOT_START_TIME = datetime.time(9)
+TIMESLOT_START_TIME = (
+    settings, 'SWINGTIME_TIMESLOT_START_TIME', datetime.time(9)
+)
 
 # A datetime.timedelta value indicating the offset value from
 # TIMESLOT_START_TIME for creating time slot grids and form selectors. The for
@@ -19,17 +26,24 @@ TIMESLOT_START_TIME = datetime.time(9)
 # 1:30am (01:30), in which case a value of datetime.timedelta(hours=10.5)
 # could be specified to indicate that the 1:30 represents the following date's
 # time and not the current date.
-TIMESLOT_END_TIME_DURATION = datetime.timedelta(hours=+8)
+TIMESLOT_END_TIME_DURATION = (
+    settings, 'SWINGTIME_TIMESLOT_END_TIME_DURATION',
+    datetime.timedelta(hours=+8)
+)
 
 # Indicates a minimum value for the number grid columns to be shown in the time
 # slot table.
-TIMESLOT_MIN_COLUMNS = 4
+TIMESLOT_MIN_COLUMNS = (settings, 'SWINGTIME_TIMESLOT_MIN_COLUMNS', 4)
 
 # Indicate the default length in time for a new occurrence, specifed by using
 # a datetime.timedelta object
-DEFAULT_OCCURRENCE_DURATION = datetime.timedelta(hours=+1)
+DEFAULT_OCCURRENCE_DURATION = (
+    settings, 'SWINGTIME_DEFAULT_OCCURRENCE_DURATION',
+    datetime.timedelta(hours=+1)
+)
 
 # If not None, passed to the calendar module's setfirstweekday function.
-CALENDAR_FIRST_WEEKDAY = 6
+CALENDAR_FIRST_WEEKDAY = (settings, 'SWINGTIME_CALENDAR_FIRST_WEEKDAY', 6)
 
-# Override in your settings
+# The event model to use if you created a custom one based on AbstractEvent
+EVENT_MODEL = (settings, 'SWINGTIME_EVENT_MODEL', 'swingtime.models.Event')
