41e0434c6d3a24896fa8a97c97336293cca340df
katy lavallee <katylava@gmail.com>
2013-08-23 11:55:23 -0500

    Remove demo project, which won't work now anyway

--- demo/demo_site/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-sys.stdout = sys.stderr
-sys.path.extend(['/var/www/swingtime'])
-os.environ['DJANGO_SETTINGS_MODULE'] = 'demo.settings'
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
-
